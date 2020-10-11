9778b5ea310feb2e11e9b5f3ccf3b75f81f42ca8
Yifan-NUC <zyfinori@gmail.com>
2020-10-11 23:15:56 +0800

    Optimize comment area of blog detail page

--- frontend/goblog-frontend/src/pages/blog/BlogDetail.vue
+++ frontend/goblog-frontend/src/pages/blog/BlogDetail.vue
@@ -1,6 +1,11 @@
 <template>
     <q-page padding>
         <div class="column blog-detail-container q-mt-md q-mx-auto">
+            <message-banner :message="message" :color="messageBannerColor" v-if="showMessage">
+                <template v-slot:action>
+                    <q-btn flat color="white" label="OK" @click="closeMessageBox" />
+                </template>
+            </message-banner>
             <div class="row">
                 <div class="col-9">
                     <div class="text-h4">
@@ -41,32 +46,24 @@
             </div>
 
             <!-- comment area -->
-            <q-list bordered padding class="q-mt-md rounded-borders">
+            <q-list bordered class="q-mt-md rounded-borders">
                 <div v-for="(comment, index) in comments" :key="comment.comment_id">
-                    <q-item>
-                        <q-item-section top avatar>
-                            <q-avatar>
-                                <img :src="comment.avatar_url" alt="">
-                            </q-avatar>
-                        </q-item-section>
-
+                    <q-item class="q-pa-md">
                         <q-item-section>
                             <q-item-label>{{comment.nickname}}</q-item-label>
                             <q-item-label caption>{{comment.content}}</q-item-label>
                         </q-item-section>
-
                         <q-item-section side top>
                             <q-item-label caption>{{new Date(comment.create_at).toLocaleString()}}</q-item-label>
                         </q-item-section>
                     </q-item>
-                    <q-separator spaced inset="item" v-if="index < comments.length - 1" />
+                    <q-separator v-if="index < comments.length - 1" />
                 </div>
                 <q-separator spaced v-if="comments.length > 0"/>
                 <!-- add comment area -->
                 <div class="q-mt-md q-mx-md">
                     <div class="text-h6">Leave Your Comment Here</div>
                     <q-input label="Nickname" v-model="commenterNickname"></q-input>
-                    <q-input label="Avatar Url" v-model="commenterAvatarUrl"></q-input>
                     <q-input label="Email" v-model="commenterEmail"></q-input>
                     <q-input label="Your Thoughts" type="textarea" v-model="commentContent"></q-input>
                     <q-btn color="primary" label="Submit" class="float-right q-mt-md" @click="submitCommentBtnClick"/>
@@ -78,8 +75,12 @@
 
 <script>
     import axios from 'axios'
+    import MessageBanner from 'components/MessageBanner.vue'
     export default {
         name: 'BlogDetail',
+        components: {
+            MessageBanner
+        },
         data () {
             return {
                 blog: '',
@@ -89,8 +90,12 @@
                 comments: [],
                 commenterNickname: '',
                 commenterEmail: '',
-                commenterAvatarUrl: '',
-                commentContent: ''
+                commentContent: '',
+                // message banner related data
+                message: '',
+                messageBannerColor: '',
+                showMessage: false,
+                timer: null
             }
         },
         methods: {
@@ -110,25 +115,45 @@
                 }
             },
             submitCommentBtnClick () {
-                const submitCommentRequest = {
-                    blog_id: this.blog.blog_id,
-                    nickname: this.commenterNickname,
-                    email: this.commenterEmail,
-                    avatar_url: this.commenterAvatarUrl,
-                    content: this.commentContent,
-                    create_at: new Date()
-                }
-                axios.post('/api/comments', submitCommentRequest)
+                if (this.commenterNickname === '') {
+                    this.setMessageAndColor('Nickname should not be empty!', this.consts.MESSAGE_BOX_COLOR_WARNING)
+                } else if (this.commenterEmail === '') {
+                    this.setMessageAndColor('Email should not be empty!', this.consts.MESSAGE_BOX_COLOR_WARNING)
+                } else if (this.commentContent === '') {
+                    this.setMessageAndColor('Comment content should not be empty!', this.consts.MESSAGE_BOX_COLOR_WARNING)
+                } else {
+                    const submitCommentRequest = {
+                        blog_id: this.blog.blog_id,
+                        nickname: this.commenterNickname,
+                        email: this.commenterEmail,
+                        content: this.commentContent,
+                        create_at: new Date()
+                    }
+                    axios.post('/api/comments', submitCommentRequest)
                     .then(this.refreshComments)
+                    this.setMessageAndColor('Successfully added the comment', this.consts.MESSAGE_BOX_COLOR_POSITIVE)
+                    this.setMessageBoxTimer(3000, this.closeMessageBox)
+                }
             },
             refreshComments () {
-                axios.get('/api/comments?blog_id=' + this.$route.params.id)
+                axios.get('/api/comments?blog_id=' + this.$route.params.blog_id)
                     .then(this.listCommentsByBlogIdSuccess)
                 // clear input area
                 this.commenterNickname = ''
-                this.commenterAvatarUrl = ''
-                this.commenterAvatarUrl = ''
+                this.commenterEmail = ''
                 this.commentContent = ''
+            },
+            setMessageAndColor (msg, color) {
+                this.message = msg
+                this.messageBannerColor = color
+                this.showMessage = true
+            },
+            setMessageBoxTimer (timeInMilliSecond, func) {
+                window.clearTimeout(this.timer)
+                this.timer = window.setTimeout(func, timeInMilliSecond)
+            },
+            closeMessageBox () {
+                this.showMessage = false
             }
         },
         mounted () {
